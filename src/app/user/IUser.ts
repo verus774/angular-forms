5b1018df71694f981e338eb2df3367a8d2f54fa5
Siarhei Ladutska <siarhei_ladutska@epam.com>
2018-12-13 16:30:46 +0300

    feat: add user model

---
+++ src/app/user/IUser.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  level: string;
+  skills: string[];
+  areasToRelocate?: string[];
+}
